d1b31ef1f351b5ae8d836f5c917da8694db9b523
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-02 18:25:43 +0100

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import logo from '../assets/LOGO.svg'
+
+const Logo = styled.img`
+  position: absolute;
+  height: 47px;
+  width: 145px;
+  margin: 20px;
+`
 
 const StyledLink = styled(Link)`
-  font-family: var(--font-primary);
+  text-transform: uppercase;
   font-size: 1rem;
   line-height: 17px;
   padding-left: 10px;
@@ -13,14 +21,18 @@ const StyledLink = styled(Link)`
 const NavBar = styled.span`
   display: flex;
   flex-direction: row-reverse;
+  padding: 38px 20px;
 `
 
 function Header() {
   return (
-    <NavBar>
-      <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/about">A Propos</StyledLink>
-    </NavBar>
+    <header>
+      <Logo src={logo} alt="logo" />
+      <NavBar>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/about">A Propos</StyledLink>
+      </NavBar>
+    </header>
   )
 }
 
