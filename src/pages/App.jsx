3508b23d7101ce5a5884c3edbd7a4496928789cc
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-02 14:30:02 +0100

    organising files

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,8 @@
-import './styles/App.css'
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header'
-import About from './pages/About'
+import Header from '../components/header/Header'
+import About from './about/About'
 
 function App() {
   return (
