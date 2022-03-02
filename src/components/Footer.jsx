9c65e4f196dc971fb54fc17d3e26fe559da8bec6
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-02 19:12:01 +0100

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
+import styled from 'styled-components'
+import logo from '../assets/LOGO.svg'
+
+const FooterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: black;
+  padding: 62px 0 62px 0;
+`
+const Logo = styled.img`
+  height: 47px;
+  width: 145px;
+  margin: auto;
+  margin-bottom: 30px;
+
+  filter: brightness(0) invert(1);
+`
+
+const FooterText = styled.span`
+  margin: auto;
+
+  color: var(--color-secondary);
+`
+
 function Footer() {
   return (
-    <div>
-      <h1>KASA LOGO</h1>
-      <h2>© 2020 Kasa. All rights reserved</h2>
-    </div>
+    <FooterBox>
+      <Logo src={logo} alt="logo" />
+      <FooterText>© 2020 Kasa. All rights reserved</FooterText>
+    </FooterBox>
   )
 }
 
