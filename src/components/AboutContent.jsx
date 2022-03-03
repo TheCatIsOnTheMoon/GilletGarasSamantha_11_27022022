45346c7b4ea57752bc67c2e4517543020dc7f5a1
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-03 17:58:03 +0100

    refacto

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-import { aboutContent } from '../data/aboutdata'
 import arrow from '../assets/arrow.svg'
 
 const DetailsBox = styled.details`
@@ -25,6 +24,25 @@ const Arrow = styled.img``
 
 //https://thewebdev.info/2021/11/15/how-to-change-the-style-of-a-button-on-click-with-react/
 //https://stackoverflow.com/questions/69345912/how-can-i-import-the-const-data-from-another-file-using-promise-react
+
+const aboutContent = [
+  {
+    title: 'Fiabilité',
+    text: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+  },
+  {
+    title: 'Respect',
+    text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+  },
+  {
+    title: 'Service',
+    text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+  },
+  {
+    title: 'Sécurité',
+    text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  },
+]
 
 function AboutContent() {
   const [cls, setCls] = useState('rotate0')
